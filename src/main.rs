5395f10d270f59ed276181365ffee6f8f84ed4fb
kyle <kyle@leastaction.me>
2023-05-18 21:34:23 -0700

    Adding substitutions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use clap::Parser;
-
 use std::fs::{self, File};
-use std::io::{self, Read};
+use std::io::{self, Read, Error, ErrorKind};
 use std::env;
 use std::path::PathBuf;
 use std::process::Command;
@@ -12,16 +11,25 @@ struct Cli {
     /// Alias you want to use with easy_alias.
     alias: Option<String>,
 
-    /// Bash script assigned to alias, enclosed in quotes if it contains spaces.
+    /// Bash script assigned to alias, enclosed 
+    /// in quotes if it contains spaces.
+    #[arg(verbatim_doc_comment)]
     cmd: Option<String>,
 
-    /// Remove provided alias if flag is present
+    /// Remove provided alias.
     #[arg(short, default_value_t = false)]
     remove: bool,
 
-    /// List aliases
+    /// List aliases.
     #[arg(short, default_value_t = false)]
     list: bool,
+
+    /// Use this flag to pass substitutions 
+    /// in the form "x<value>,y<value>..."
+    /// If the value contains a comma or dollar 
+    /// sign, escape it with a \\
+    #[arg(short, verbatim_doc_comment)]
+    subs: Option<String>
 }
 
 impl Cli {
@@ -60,7 +68,7 @@ impl Cli {
         } else if let Some(_) = self.alias {
             self.run_command()
         } else {
-            println!("Invalid input. Try using the --help flag.");
+            println!("Invalid input. Try using the -h or --help flags.");
         }
     }
 
@@ -141,32 +149,64 @@ impl Cli {
     }
 
     fn run_command(&self) {
-        let cmd = self.get_full_cmd();
-        let mut cmd_quote = "\"".to_string();
-        cmd_quote.push_str(&cmd);
-        cmd_quote.push('"');
-        let output = Command::new("sh")
-            .arg("-c")
-            .arg(cmd)
-            .status()
-            .expect("Failed to execute command.");
-        if !output.success() {
-            println!("Error! {}", output);
+        let res = self.get_full_cmd();
+        if let Ok(cmd) = res {
+            let mut cmd_quote = "\"".to_string();
+            cmd_quote.push_str(&cmd);
+            cmd_quote.push('"');
+            let output = Command::new("sh")
+                .arg("-c")
+                .arg(cmd)
+                .status()
+                .expect("Failed to execute command.");
+            if !output.success() {
+                println!("Error! {}", output);
+            }
+        } else if let Err(x) = res {
+            println!("{}", x);
         }
     }
 
-    fn get_full_cmd(&self) -> String {
+    fn expand_subs(&self, line: &str) -> Result<String, Error> {
+        if let Some(subs) = self.subs.clone() {
+            let mut new_line = line.clone().to_string();
+            let safe_subs = subs.replace("\\,", "::COMMA::");
+            let sub_iter = safe_subs.split(',').map(|val| val.replace("::COMMA::", ","));
+            for sub in sub_iter {
+                let mut new_val = String::new();
+                let mut str_to_replace = "**".to_string();
+                for (i, char) in sub.chars().enumerate() {
+                    if i == 0 {
+                        str_to_replace.push(char);
+                    } else if i > 1 {
+                        new_val.push(char);
+                    }
+                }
+                new_line = new_line.replace(&str_to_replace, &new_val);
+            }
+            println!("Command after subs: {}", &new_line);
+            return Ok(new_line)
+        } else {
+            if let None = line.find("**") {
+                return Ok(line.to_string())
+            } else {
+                let err_string = format!("Error!{}There are substitutions you must make for any variables following **{}The command for this alias is: {}", '\n', '\n', &line);
+                return Err(Error::new(ErrorKind::InvalidInput, err_string))
+            }
+        }
+    }
+
+    fn get_full_cmd(&self) -> Result<String, Error> {
         let (config, _) = self.read_config().unwrap();
         let mut start_str = self.alias.clone().unwrap();
         start_str.push_str("::");
         for line in config.split('\n') {
             if line.starts_with(&start_str) {
-                return line.split("::").nth(1).unwrap().to_string();
+                let full_cmd = self.expand_subs(line.split("::").nth(1).unwrap());
+                return full_cmd;
             }
         }
-        println!("\nError! Alias not found.");
-        self.list_aliases();
-        return "".to_string()
+        return Err(Error::new(ErrorKind::InvalidInput, "Alias not found! List aliases with the -l flag."))
     }
 }
 
